30787dd75bdc8fc4d6f3098f7375108213b310e7
Abhijith <abhijithsomasekhar@outlook.com>
2022-04-19 22:01:23 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+	<link rel="stylesheet" href="style.css">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<h1> E-Learn</h1>
+	<div id="signin_form">
+		<a href="instructor/index.php"><input type="button" value="Instructor"></a>
+		<a href="student/index.php"><input type="button" value="Student"></a>
+	</div>
+</body>
+</html>
